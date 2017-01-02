ee510ac5d98cf5cbe8285c2a3aee7f6d5a02ff99
rdinizcal <rdinizcal@gmail.com>
2017-01-02 20:45:01 -0200

    REST

--- app/src/main/java/br/unb/unbsolidaria/views/voluntary/ViewOpportunities.java
+++ app/src/main/java/br/unb/unbsolidaria/views/voluntary/ViewOpportunities.java
@@ -11,10 +11,16 @@ import android.view.ViewGroup;
 import java.util.List;
 
 import br.unb.unbsolidaria.R;
+import br.unb.unbsolidaria.communication.OpportunityService;
+import br.unb.unbsolidaria.communication.RestCommunication;
+import br.unb.unbsolidaria.entities.Organization;
 import br.unb.unbsolidaria.utils.OpportunitiesAdapter;
 import br.unb.unbsolidaria.entities.Opportunity;
 import br.unb.unbsolidaria.entities.User;
 import br.unb.unbsolidaria.persistence.DBHandler;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ViewOpportunities extends Fragment {
 
@@ -32,6 +38,7 @@ public class ViewOpportunities extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View parentView = inflater.inflate(R.layout.fragment_opportunities_list, container, false);
+        OpportunityService opportunityService = RestCommunication.createService(OpportunityService.class);
 
         mRecyclerView = (RecyclerView) parentView.findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -39,10 +46,23 @@ public class ViewOpportunities extends Fragment {
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        DBHandler bd = DBHandler.getInstance();
-        mList = bd.getOpportunities();
+        //DBHandler bd = DBHandler.getInstance();
+        Call<List<Opportunity>> call = opportunityService.getOpportunities();
 
-        db_interface = DBHandler.getInstance();
+        call.enqueue(new Callback<List<Opportunity>>() {
+            @Override
+            public void onResponse(Call<List<Opportunity>> call, Response<List<Opportunity>> response) {
+                mList = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<Opportunity>> call, Throwable t) {
+
+            }
+        });
+        //mList = bd.getOpportunities();
+
+        //db_interface = DBHandler.getInstance();
 
         Bundle box = this.getArguments();
 

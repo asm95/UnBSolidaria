ca88f872029c44e570e9fd16ffa5e2452e740c95
Eduardo <eduardocalandrini@hotmail.com>
2017-01-09 20:58:45 -0200

    Signup Create Signup Method Create Retrofit class  and Create Signup class only for buffer

---
+++ app/src/main/java/br/unb/unbsolidaria/entities/RetrofitResponse.java
@@ -0,0 +1,13 @@
+package br.unb.unbsolidaria.entities;
+
+/**
+ * Created by eduar on 09/01/2017.
+ */
+
+public class RetrofitResponse {
+    private String response;
+
+    public String getResponse(){
+        return this.response;
+    }
+}

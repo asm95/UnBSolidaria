fa997311f747a2f82c214f0ee7e677ccbb7327d6
asm95 <crissonicx2@gmail.com>
2017-01-17 17:07:13 -0200

    (Update) Tela de Oportunidade
    - (UI) (Enhancement) Nome e "Sobrenome" da Organização é mostrada no lugar de seu link
    - (UI) (Addition) Adicionado uma tela de Erro Fatal. Ela não deveria ser necessária, porém é por causa da robustez do código atual
    - (Git) (Addition) (OSX) Ignorar arquivos com extensão .DS_STORE
    
    TODO:
    
    - Adicionar mecanismo para participar de uma oportunidade.
    Solução 1: (Server) Adicionar uma API no servidor porque a ação requer um token CSRF e sessionID. Estes dois últimos são salvos como cookies.
    Solução 2: Utilizar o token retornado no login
    Solução 3: Utilizar a *força bruta*: fazer reverse engeneering (RE) e mandar requerimentos http com esses cookies.

--- app/src/main/java/br/unb/unbsolidaria/views/voluntary/OpportunityAcitivity.java
+++ app/src/main/java/br/unb/unbsolidaria/views/voluntary/OpportunityAcitivity.java
@@ -7,21 +7,32 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import br.unb.unbsolidaria.R;
 import br.unb.unbsolidaria.Singleton;
+import br.unb.unbsolidaria.communication.RestCommunication;
 import br.unb.unbsolidaria.entities.Opportunity;
+import br.unb.unbsolidaria.entities.User;
 import br.unb.unbsolidaria.entities.Voluntary;
 import br.unb.unbsolidaria.utils.ImageHelper;
-import br.unb.unbsolidaria.persistence.DBHandler;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 
 public class OpportunityAcitivity extends AppCompatActivity {
@@ -37,16 +48,23 @@ public class OpportunityAcitivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //loggedVoluntary = (Voluntary)getIntent().getSerializableExtra(ViewOpportunities.VIEW_MESSAGE);
+
+        int id = getIntent().getIntExtra("id", 0) - 1;
+        List<Opportunity> mList = singleton.getOpportunityList();
+        Opportunity mOpportunityInfo = mList.get(id);
+        if (mOpportunityInfo == null){
+            Log.e("ViewOpportunity", "Could not retrieve Opportunity ID (" + id + ") from Singleton");
+            setContentView(R.layout.activity_fatal_error);
+            TextView errText = (TextView) findViewById(R.id.errText);
+            errText.setText("Não foi possível obter dados desta oportunidade.");
+            return;
+        }
+
         setContentView(R.layout.activity_opportunity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.oportunidateToolbar_op);
         setSupportActionBar(toolbar);
-
-        loggedVoluntary = (Voluntary)getIntent().getSerializableExtra(ViewOpportunities.VIEW_MESSAGE);
-        int id = getIntent().getIntExtra("id", 0) - 1;
-        List<Opportunity> mList = singleton.getOpportunityList();
-
-        DBHandler bd = DBHandler.getInstance();
-        //Opportunity opportunity = bd.getOpportunity(id);
 
         ImageView logo = (ImageView) findViewById(R.id.iv_op_logo);
         TextView description = (TextView) findViewById(R.id.tv_op_description);
@@ -57,21 +75,27 @@ public class OpportunityAcitivity extends AppCompatActivity {
         TextView end = (TextView) findViewById(R.id.tv_op_end);
 
 
-        description.setText( getString(R.string.ov_description, mList.get(id).getDescription()) );
-        org.setText( getString(R.string.ov_org, mList.get(id).getOrganizacao()) );
+        description.setText( getString(R.string.ov_description, mOpportunityInfo.getDescription()) );
 
-        local.setText( getString(R.string.ov_local, mList.get(id).getLocal()) );
-        if (mList.get(id).getLocal() == null){
-            local.setText("Não especificado");
+        String orgText = mOpportunityInfo.getOrganizacao();
+        if (orgText != null){
+            Pattern pattern = Pattern.compile("(/users/)([0-9]*)");
+            Matcher matcher = pattern.matcher(orgText);
+            if (matcher.find()){
+                int orgID = Integer.parseInt(matcher.group(2));
+                setOrganizationName(org, orgID);
+            }
         }
-        vagas.setText( getString(R.string.ov_vagas, String.valueOf(mList.get(id).getVagas())) );
+        org.setText( getString(R.string.ov_org, "") );
 
-        start.setText( getString(R.string.ov_dateStart, mList.get(id).getData_inicio()) );
-        end.setText( getString(R.string.ov_dateEnd, mList.get(id).getData_fim()) );
+        local.setText( getString(R.string.ov_local, mOpportunityInfo.getLocal()) );
+        if (mList.get(id).getLocal() == null){
+            local.setText(getString(R.string.ov_local, getString(R.string.ov_undefinedField)));
+        }
+        vagas.setText( getString(R.string.ov_vagas, String.valueOf(mOpportunityInfo.getVagas())) );
 
-        scale = this.getResources().getDisplayMetrics().density;
-        width = this.getResources().getDisplayMetrics().widthPixels - (int) (14 * scale + 0.5f);
-        height = (width / 16) * 9;
+        start.setText( getString(R.string.ov_dateStart, mOpportunityInfo.getData_inicio()) );
+        end.setText( getString(R.string.ov_dateEnd, mOpportunityInfo.getData_fim()) );
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             logo.setImageResource(mList.get(id).getPhoto());
@@ -87,18 +111,17 @@ public class OpportunityAcitivity extends AppCompatActivity {
         getSupportActionBar().setTitle(mList.get(id).getTitle());
 
 
-
-        final Button button = (Button) findViewById(R.id.oportunidadeActivity_actionConfirm);
+        final Button joinButton = (Button) findViewById(R.id.oportunidadeActivity_actionConfirm);
         if ( mList.get(id).getVagas() <= 0 )
-            button.setEnabled(false);
+            joinButton.setEnabled(false);
 
         // no user is logged
         if(loggedVoluntary == null){
-            button.setEnabled(false);
-            button.setVisibility(View.GONE);
+            joinButton.setEnabled(false);
+            joinButton.setVisibility(View.GONE);
         }
 
-        button.setOnClickListener(new View.OnClickListener() {
+        joinButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //TODO: Adicionar algum mecanismo para gerenciar eventos em que o usuário se candidatou (Activity, pop-up,...)
                 Snackbar.make(findViewById(android.R.id.content), "Participação Confirmada!", Snackbar.LENGTH_SHORT).show();
@@ -130,4 +153,37 @@ public class OpportunityAcitivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    void setOrganizationName(final TextView output, int orgID){
+        OrganizationService orgHandler = RestCommunication.createService(OrganizationService.class);
+
+        Call<User> userFetch = orgHandler.getOrganizationByID(orgID);
+        userFetch.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User org = response.body();
+                if (org != null){
+                    Log.i("REST", "User response: " + response.toString());
+                    String fn, ln;
+                    fn = org.getFirst_name();
+                    ln = org.getLast_name();
+                    output.setText(getString(R.string.ov_org, fn + " " + ln));
+                    ProgressBar progressStatus = (ProgressBar)findViewById(R.id.pb_orgNameProgressStatus);
+                    if(progressStatus != null){
+                        progressStatus.setVisibility(View.GONE);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                output.setText(getString(R.string.ov_undefinedField));
+            }
+        });
+    }
+}
+
+interface OrganizationService {
+    @GET("users/{id}")
+    Call<User> getOrganizationByID(@Path("id") int orgID);
 }
